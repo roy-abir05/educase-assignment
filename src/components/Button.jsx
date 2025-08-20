982394cd60878b21567fb74e3a2c73f74bc9ff98
roy-abir05 <royabirdhn@gmail.com>
2025-08-20 20:30:11 +0530

    feat: add home page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
-const Button = ({ children, className, highlight }) => {
-
+const Button = ({ children, className, highlight, ...props }) => {
   return (
     <button
-      className={`w-[335px] h-[46px] rounded-[6px] text-center font-[Rubik,sans-serif] font-medium leading-0 ${highlight?"bg-[#6C25FF] text-[#FFFFFF]":"bg-[#6C25FF4B] text-[#1D2226]"} ${className}`}
+      {...props}
+      className={`w-[335px] h-[46px] rounded-[6px] text-center font-[Rubik,sans-serif] font-medium leading-0 transition-all duration-300 cursor-pointer ${
+        highlight
+          ? "bg-[#6C25FF] text-[#FFFFFF] hover:bg-[#6e25ffee] active:bg-[#6C25FFCC]"
+          : "bg-[#6C25FF4B] text-[#1D2226] hover:bg-[#6C25FF66] active:bg-[#6C25FF99]"
+      } ${className}`}
     >
       {children}
     </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
+import Button from "../components/Button";
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="parent-container flex flex-col justify-end items-start">
+      <div className="w-[250px] h-[120px] mb-2">
+        <div className="title font-medium mb-1.5">Welcome to PopX</div>
+        <div className="subtitle">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        </div>
+      </div>
+      <div>
+        <Link to="/signup">
+          <Button className="mb-2.5" highlight>
+            Create Account
+          </Button>
+        </Link>
+        <Link to="/login">
+          <Button Link="/login">Already Registered? Login</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
